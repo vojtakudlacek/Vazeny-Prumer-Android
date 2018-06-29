302a4635119b4a3b976fe90251e5248d9e1e6fbb
Vojta <spongeblockcz@gmail.com>
2018-06-29 20:29:51 +0200

    Added List of grades

--- app/src/main/java/cz/soft/vojta_kudlacek/VazenyPrumerAndroid/MainActivity.kt
+++ app/src/main/java/cz/soft/vojta_kudlacek/VazenyPrumerAndroid/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,15 +15,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var gradesList = findViewById<TextView>(R.id.gradeList)
+        var weightList = findViewById<TextView>(R.id.weightList)
+        gradesList.text = gradesList.text.toString() + ": \n"
+        weightList.text = weightList.text.toString() + ": \n"
 
     }
 
     fun Add(view:View) {
+
         var gradeET = findViewById<EditText>(R.id.grade)
         var weightET = findViewById<EditText>(R.id.weight)
+        var gradesList = findViewById<TextView>(R.id.gradeList)
+        var weightList = findViewById<TextView>(R.id.weightList)
+
+        gradeList.text = (gradesList.text.toString() + gradeET.text.toString() + "\n")
+        weightList.text = (weightList.text.toString() + weightET.text.toString() + "\n")
+
         array += Grade(gradeET.text.toString().toByte(), weightET.text.toString().toByte())
         var diameterTW = findViewById<TextView>(R.id.diameter)
         var diameter:Float =  getDiameter()
+
         if (diameter<=2.5f)
         {
             diameterTW.setTextColor(ColorStateList.valueOf(Color.GREEN))
@@ -34,6 +47,7 @@ class MainActivity : AppCompatActivity() {
             diameterTW.setTextColor(ColorStateList.valueOf(Color.BLACK))
         }
         diameterTW.text = diameter.toString()
+
     }
 
     fun getDiameter() : Float
